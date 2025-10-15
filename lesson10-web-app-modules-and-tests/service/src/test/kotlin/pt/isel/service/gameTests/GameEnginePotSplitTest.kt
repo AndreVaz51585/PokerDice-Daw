e85057b89cd267d1c528d52f98c486a4ed6059ea
DiogoTeixeira3 <diteixeira04@gmail.com>
2025-10-15 10:11:51 +0100

    all game tests done

---
+++ lesson10-web-app-modules-and-tests/service/src/test/kotlin/pt/isel/service/gameTests/GameEnginePotSplitTest.kt
@@ -0,0 +1,31 @@
+package pt.isel.service.gameTests
+
+import pt.isel.domain.Game.Face
+import pt.isel.domain.Game.pokerDice.*
+import pt.isel.domain.Game.Round.RoundState
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class GameEnginePotSplitTest {
+
+    private val seq = generateSequence {
+        listOf(Face.KING, Face.KING, Face.KING, Face.KING, Face.KING)
+    }.flatten().iterator()
+    private fun roll(): Face = seq.next()
+
+    @Test
+    fun pot_split_between_two_equal_five_of_a_kind() {
+        var g = Game(9, hostId = 1, ante = 5, maxPlayers = 2, totalRounds = 1)
+        g = GameEngine.apply(g, Command.Join(1), ::roll)
+        g = GameEngine.apply(g, Command.Join(2), ::roll)
+        g = GameEngine.apply(g, Command.Start(1), ::roll)
+        g = GameEngine.apply(g, Command.Roll(1), ::roll)
+        g = GameEngine.apply(g, Command.FinishTurn(1), ::roll)
+        g = GameEngine.apply(g, Command.Roll(2), ::roll)
+        g = GameEngine.apply(g, Command.FinishTurn(2), ::roll)
+        assertEquals(RoundState.SCORING, g.rounds.last().state)
+        g = GameEngine.apply(g, Command.NextRound(1), ::roll)
+        assertEquals(5, g.balances[1])
+        assertEquals(5, g.balances[2])
+    }
+}
---
+++ lesson10-web-app-modules-and-tests/service/src/test/kotlin/pt/isel/service/gameTests/MoneyModelsTest.kt
@@ -0,0 +1,27 @@
+package pt.isel.service.gameTests
+
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+import pt.isel.domain.Game.money.*
+
+class MoneyModelsTest {
+
+    @Test
+    fun ante_validation() {
+        assertFailsWith<IllegalArgumentException> { Ante(0, matchId = 1, roundNumber = 1) }
+        Ante(5, matchId = 1, roundNumber = 1)
+    }
+
+    @Test
+    fun pot_validation() {
+        assertFailsWith<IllegalArgumentException> { Pot(matchId = 0, roundNumber = 1) }
+        assertFailsWith<IllegalArgumentException> { Pot(matchId = 1, roundNumber = 0) }
+        Pot(matchId = 1, roundNumber = 1, total = 0)
+    }
+
+    @Test
+    fun wallet_validation() {
+        assertFailsWith<IllegalArgumentException> { Wallet(userId = 1, currentBalance = -1) }
+        Wallet(userId = 2, currentBalance = 0)
+    }
+}
